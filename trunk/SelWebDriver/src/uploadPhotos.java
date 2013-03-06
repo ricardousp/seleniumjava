3146a7af194fc8efccf693af0da9432c4aea48bb
payalgarg01@gmail.com <payalgarg01@gmail.com@30acd3d0-6252-4e06-4740-64d4948c5a8a>
2013-03-06 21:49:46 +0000

    updated print statement

---
+++ trunk/SelWebDriver/src/uploadPhotos.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class uploadPhotos{
+	
+	WebDriver myD;
+
+@Before
+public void setUp() throws Exception{
+	myD = new FirefoxDriver();
+	myD.navigate().to("http://www.gmail.com/");
+}
+@Test
+public void Test1() throws Exception{
+	
+	myD.findElement(By.id("email")).sendKeys("payalgarg01@gmail.com");
+	myD.findElement(By.id("pass")).sendKeys("laddooji");
+	myD.findElement(By.id("u_0_4")).click();
+	myD.findElement(By.linkText("Payal Garg")).click();
+	myD.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]/div/div[2]/div[2]/div/div/div[2]/div/a/span")).click();
+	myD.findElement(By.id("u_e_3")).click();
+	System.out.println("captured image");
+	
+}
+	
+@After
+public void TearDown(){
+	System.out.println("done");
+}
+}
